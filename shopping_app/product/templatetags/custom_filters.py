8ca6291cfccd250c8c2ea2bf0de46bd6d5de41bf
ali <alimdv74.am@outlook.com>
2023-05-23 22:07:40 +0330

    add custom_filter to better display the phone number

--- shopping_app/product/templatetags/custom_filters.py
+++ shopping_app/product/templatetags/custom_filters.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.filter
 def price(value):
     return f"{round(value/10):,} تومان"
+
+
+@register.filter
+def phone(value):
+    return f"0{value.national_number}" if value else value
